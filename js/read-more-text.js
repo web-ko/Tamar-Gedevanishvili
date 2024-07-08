38887973b3eab83cd6c221f587836abce237860c
web-ko <79904428+web-ko@users.noreply.github.com>
2024-07-08 13:31:34 +0400

    update read-more-text.js

--- js/read-more-text.js
+++ js/read-more-text.js
@@ -1,17 +1,36 @@
-const parentContainer = document.querySelector('.read_more');
+// const parentContainer = document.querySelector('.read_more');
 
-parentContainer.addEventListener('click', event=>{
+// parentContainer.addEventListener('click', event=>{
 
-	const current = event.target;
+// 	const current = event.target;
 
-	const isReadMoreBtn = current.className.includes('read-more-btn');
+// 	const isReadMoreBtn = current.className.includes('read-more-btn');
 
-	if(!isReadMoreBtn) return;
+// 	if(!isReadMoreBtn) return;
 
-	const currentText = event.target.parentNode.querySelector('.read-more-text');
+// 	const currentText = event.target.parentNode.querySelector('.read-more-text');
 
-	currentText.classList.toggle('read-more-text--show');
+// 	currentText.classList.toggle('read-more-text--show');
 
-	current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
+// 	current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
 
-})
+// })
+
+
+
+ $(".read-more-btn").click(function(){
+ 	$(".read-more-btn").toggleClass("hidden");
+ 	$(".read-more-text").toggleClass("read-more-text--show");
+ 	$(".thr_dots").toggleClass("hidden");
+ 	$(".more_paragr").toggleClass("show");
+ 	$(".read-less-btn").toggleClass("show");
+ });
+ $(".read-less-btn").click(function(){
+ 	$(".read-more-btn").removeClass("hidden");
+ 	$(".read-more-text").removeClass("read-more-text--show");
+ 	$(".thr_dots").removeClass("hidden");
+ 	$(".more_paragr").removeClass("show");
+ 	$(".read-less-btn").removeClass("show");
+ });
+
+
