7b9e4da13019b0025563099e9d11e94d330a33a2
web-ko <79904428+web-ko@users.noreply.github.com>
2024-07-08 14:54:13 +0400

    update read-more-text.js

--- js/read-more-text.js
+++ js/read-more-text.js
@@ -18,19 +18,40 @@
 
 
 
- $(".read-more-btn").click(function(){
- 	$(".read-more-btn").toggleClass("hidden");
- 	$(".read-more-text").toggleClass("read-more-text--show");
- 	$(".thr_dots").toggleClass("hidden");
- 	$(".more_paragr").toggleClass("show");
- 	$(".read-less-btn").toggleClass("show");
- });
- $(".read-less-btn").click(function(){
- 	$(".read-more-btn").removeClass("hidden");
- 	$(".read-more-text").removeClass("read-more-text--show");
- 	$(".thr_dots").removeClass("hidden");
- 	$(".more_paragr").removeClass("show");
- 	$(".read-less-btn").removeClass("show");
- });
+ // $(".read-more-btn").click(function(){
+ // 	$(".read-more-btn").toggleClass("hidden");
+ // 	$(".read-more-text").toggleClass("read-more-text--show");
+ // 	$(".thr_dots").toggleClass("hidden");
+ // 	$(".more_paragr").toggleClass("show");
+ // 	$(".read-less-btn").toggleClass("show");
+ // });
+ // $(".read-less-btn").click(function(){
+ // 	$(".read-more-btn").removeClass("hidden");
+ // 	$(".read-more-text").removeClass("read-more-text--show");
+ // 	$(".thr_dots").removeClass("hidden");
+ // 	$(".more_paragr").removeClass("show");
+ // 	$(".read-less-btn").removeClass("show");
+ // });
 
 
+// When I click "Read less" the scroll position remains at the location of the "Read more" button
+let scrollPosition;
+
+$(".read-more-btn").click(function() {
+    scrollPosition = $(window).scrollTop(); // Store the current scroll position
+    $(".read-more-btn").toggleClass("hidden");
+    $(".read-more-text").toggleClass("read-more-text--show");
+    $(".thr_dots").toggleClass("hidden");
+    $(".more_paragr").toggleClass("show");
+    $(".read-less-btn").toggleClass("show");
+});
+
+$(".read-less-btn").click(function() {
+    $(".read-more-btn").removeClass("hidden");
+    $(".read-more-text").removeClass("read-more-text--show");
+    $(".thr_dots").removeClass("hidden");
+    $(".more_paragr").removeClass("show");
+    $(".read-less-btn").removeClass("show");
+    $(window).scrollTop(scrollPosition); // Restore the scroll position
+});
+// End of When I click "Read less" the scroll position remains at the location of the "Read more" button
