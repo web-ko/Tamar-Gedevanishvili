a5762a87657c0ba9bb5ef83de91de09d73e7e77c
web-ko <79904428+web-ko@users.noreply.github.com>
2025-01-14 13:06:36 +0400

    update script.js

--- js/script.js
+++ js/script.js
@@ -164,8 +164,27 @@ $('a[href^="#trigger-icon').on("click", function(){
 });
 
 
+// $('a[href*="#trigger-icon"]:not([href="#"])').click(function() {
+//     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+//         var target = $(this.hash);
+//         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//         if (target.length) {
+//             $('html, body').animate({
+//                 scrollTop: target.offset().top
+//             }, 500);
+//             return false;
+//         }
+//     }
+// });
+
+
 $(window).scroll(function(){
     $(".trigger-icon").toggleClass("scrolled", $(this).scrollTop() > 500);
 });
 
 // end of trigger-icon
+
+
+
+Fancybox.bind("[data-fancybox]",{
+});
