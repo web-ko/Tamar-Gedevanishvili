a4492c85dec773019f93ee198128d88a766ee9fc
web-ko <79904428+web-ko@users.noreply.github.com>
2025-03-23 18:32:02 +0400

    update read-more-text.js

--- js/read-more-text.js
+++ js/read-more-text.js
@@ -35,60 +35,7 @@
 
 
 // When I click "Read less" the scroll position remains at the location of the "Read more" button
-// let scrollPosition;
-
-// $(".read-more-btn").click(function() {
-//     scrollPosition = $(window).scrollTop(); // Store the current scroll position
-//     $(".read-more-btn").toggleClass("hidden");
-//     $(".read-more-text").toggleClass("read-more-text--show");
-//     $(".thr_dots").toggleClass("hidden");
-//     $(".more_paragr").toggleClass("show");
-//     $(".read-less-btn").toggleClass("show");
-// });
-
-// $(".read-less-btn").click(function() {
-//     $(".read-more-btn").removeClass("hidden");
-//     $(".read-more-text").removeClass("read-more-text--show");
-//     $(".thr_dots").removeClass("hidden");
-//     $(".more_paragr").removeClass("show");
-//     $(".read-less-btn").removeClass("show");
-//     $(window).scrollTop(scrollPosition); // Restore the scroll position
-// });
-
-
-
-// let scrollPosition1;
-
-// $(".read-more-btn1").click(function() {
-//     scrollPosition1 = $(window).scrollTop(); // Store the current scroll position
-//     $(".read-more-btn1").toggleClass("hidden");
-//     $(".read-more-text1").toggleClass("read-more-text--show");
-//     $(".thr_dots1").toggleClass("hidden");
-//     $(".more_paragr1").toggleClass("show");
-//     $(".read-less-btn1").toggleClass("show");
-// });
-
-// $(".read-less-btn1").click(function() {
-//     $(".read-more-btn1").removeClass("hidden");
-//     $(".read-more-text1").removeClass("read-more-text--show");
-//     $(".thr_dots1").removeClass("hidden");
-//     $(".more_paragr1").removeClass("show");
-//     $(".read-less-btn1").removeClass("show");
-//     $(window).scrollTop(scrollPosition1); // Restore the scroll position
-// });
-
-
-
-
-
-
 let scrollPosition;
-let scrollPosition1;
-
-// Function to detect if the device is Android
-function isAndroid() {
-    return /android/i.test(navigator.userAgent);
-}
 
 $(".read-more-btn").click(function() {
     scrollPosition = $(window).scrollTop(); // Store the current scroll position
@@ -105,16 +52,12 @@ $(".read-less-btn").click(function() {
     $(".thr_dots").removeClass("hidden");
     $(".more_paragr").removeClass("show");
     $(".read-less-btn").removeClass("show");
-
-    // Check if the device is Android
-    if (isAndroid()) {
-        // If on Android, set scrollTop without animation
-        $(window).scrollTop(scrollPosition);
-    } else {
-        // For other devices, keep the scroll animation
-        $("html, body").animate({ scrollTop: scrollPosition }, 0);
-    }
+    $(window).scrollTop(scrollPosition); // Restore the scroll position
 });
+
+
+
+let scrollPosition1;
 
 $(".read-more-btn1").click(function() {
     scrollPosition1 = $(window).scrollTop(); // Store the current scroll position
@@ -131,16 +74,73 @@ $(".read-less-btn1").click(function() {
     $(".thr_dots1").removeClass("hidden");
     $(".more_paragr1").removeClass("show");
     $(".read-less-btn1").removeClass("show");
-
-    // Check if the device is Android
-    if (isAndroid()) {
-        // If on Android, set scrollTop without animation
-        $(window).scrollTop(scrollPosition1);
-    } else {
-        // For other devices, keep the scroll animation
-        $("html, body").animate({ scrollTop: scrollPosition1 }, 0);
-    }
+    $(window).scrollTop(scrollPosition1); // Restore the scroll position
 });
+
+
+
+
+
+
+// let scrollPosition;
+// let scrollPosition1;
+
+// // Function to detect if the device is Android
+// function isAndroid() {
+//     return /android/i.test(navigator.userAgent);
+// }
+
+// $(".read-more-btn").click(function() {
+//     scrollPosition = $(window).scrollTop(); // Store the current scroll position
+//     $(".read-more-btn").toggleClass("hidden");
+//     $(".read-more-text").toggleClass("read-more-text--show");
+//     $(".thr_dots").toggleClass("hidden");
+//     $(".more_paragr").toggleClass("show");
+//     $(".read-less-btn").toggleClass("show");
+// });
+
+// $(".read-less-btn").click(function() {
+//     $(".read-more-btn").removeClass("hidden");
+//     $(".read-more-text").removeClass("read-more-text--show");
+//     $(".thr_dots").removeClass("hidden");
+//     $(".more_paragr").removeClass("show");
+//     $(".read-less-btn").removeClass("show");
+
+//     // Check if the device is Android
+//     if (isAndroid()) {
+//         // If on Android, set scrollTop without animation
+//         $(window).scrollTop(scrollPosition);
+//     } else {
+//         // For other devices, keep the scroll animation
+//         $("html, body").animate({ scrollTop: scrollPosition }, 0);
+//     }
+// });
+
+// $(".read-more-btn1").click(function() {
+//     scrollPosition1 = $(window).scrollTop(); // Store the current scroll position
+//     $(".read-more-btn1").toggleClass("hidden");
+//     $(".read-more-text1").toggleClass("read-more-text--show");
+//     $(".thr_dots1").toggleClass("hidden");
+//     $(".more_paragr1").toggleClass("show");
+//     $(".read-less-btn1").toggleClass("show");
+// });
+
+// $(".read-less-btn1").click(function() {
+//     $(".read-more-btn1").removeClass("hidden");
+//     $(".read-more-text1").removeClass("read-more-text--show");
+//     $(".thr_dots1").removeClass("hidden");
+//     $(".more_paragr1").removeClass("show");
+//     $(".read-less-btn1").removeClass("show");
+
+//     // Check if the device is Android
+//     if (isAndroid()) {
+//         // If on Android, set scrollTop without animation
+//         $(window).scrollTop(scrollPosition1);
+//     } else {
+//         // For other devices, keep the scroll animation
+//         $("html, body").animate({ scrollTop: scrollPosition1 }, 0);
+//     }
+// });
 
 
 
